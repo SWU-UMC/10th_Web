bf5ce1470aa77ea7c6021b3af2b95d2f4c47c944
yoona24 <estelle0204@naver.com>
2026-03-22 23:30:08 +0900

    1주차 미션1 완료

---
+++ yona/week1/ch.1/string.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const yona = '요나';
+let text = "Hello,TypeScript";
+let template = `안녕하세요, ${text}`;
+//# sourceMappingURL=string.js.map

f5c6e9be688e9c9d1bc6530ebdadb0904c538b18
nonshaman <ztw98nt4nj@privaterelay.appleid.com>
2026-03-28 08:03:48 +0900

    Chore: 2주차 미션1

--- week2/chapter-2/src/App.tsx
+++ week2/chapter-2/src/App.tsx
@@ -1,22 +1,11 @@
-import { useState } from 'react';
+import './App.css';
+import Todo from './components/Todo.tsx';
 
 function App() {
-  const[count, setCount] = useState(0);
-
-  const countUp = () => {
-    setCount(count + 1);
-  }
-
-  const countDown= () => {
-    setCount(count - 1);
-  }
-
-  return (
-    <>
-      <h1>카운트 = {count}</h1>
-      <button onClick={countUp}>+1 버튼</button>
-      <button onClick={countDown}>-1 버튼</button>
-    </>
+  return ( 
+    <div>
+      <Todo></Todo>
+    </div>
   );
 }
 

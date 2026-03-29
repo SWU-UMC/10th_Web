4b95ae93f1331d21a65f4b77407a184eb2ab0a1b
이윤정 <jjungjjung0326@naver.com>
2026-03-29 23:49:50 +0900

    refactor: 미션 1 파일 분리

--- week2/project01/src/App.tsx
+++ week2/project01/src/App.tsx
@@ -1,11 +1,85 @@
-
+import { useState } from 'react';
 import './App.css';
-import ContextPage from './context/ContextPage';
 
-export default function App() {
+type Todo = {
+  id: number;
+  text: string;
+};
+
+function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);  // 해야 할 일 목록
+  const [doneTodos, setDoneTodos] = useState<Todo[]>([]); // 완료된 목록
+  const [input, setInput] = useState(""); // 입력창 값
+
+  const complete = (todo: Todo) => {
+    setDoneTodos(doneTodos.filter((t) => t.id !== todo.id));
+  };
+
+  const doneTask = (todo: Todo) => {
+    setTodos(todos.filter((t) => t.id !== todo.id));
+    setDoneTodos([...doneTodos, todo]);
+  };
+
+  const addTodo = () => {
+    const text = input.trim();
+    if (text === "") return;
+    const newTodo: Todo = {
+      id: Date.now(),
+      text,
+    };
+    setTodos([...todos, newTodo]);
+    setInput("");
+  };
+
   return (
     <>
-      <ContextPage />
+    <div className='head'>
+      <h1>YONG TO DO</h1>
+      <div className='header'>
+        <input
+          type="text"
+          placeholder="할 일 입력"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          className='input-box'
+                
+        />
+        <button onClick={addTodo}>할 일 추가</button>
+      </div>
+      
+      <div className="container">
+        <div className="section">
+          <h2>할 일</h2>
+          <ul>
+            {todos.map((todo) => (
+              <li key={todo.id}>
+                {todo.text}
+                <button onClick={() => doneTask(todo)} 
+                  style={{ backgroundColor: "green", color: "white"}}  
+                  >완료</button>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="section">
+          <h2>완료</h2>
+          <ul>
+            {doneTodos.map((todo) => (
+              <li key={todo.id}>
+                {todo.text}
+                <button onClick={() => complete(todo)}
+                  style={{ backgroundColor: "red", color: "white"}}
+                  >삭제</button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      
+    </div> 
     </>
   );
 }
+
+export default App;

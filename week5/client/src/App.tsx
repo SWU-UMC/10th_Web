7e76998663add6b13efe317fcc51a292fbe1e97e
Administrator <kdy99ky@gmail.com>
2026-04-30 04:45:43 +0900

    feat: 구글 소셜 로그인 연동

--- week5/client/src/App.tsx
+++ week5/client/src/App.tsx
@@ -3,18 +3,20 @@ import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 import ProtectedRoute from './components/ProtectedRoute'; 
 import MyPage from './pages/MyPage'; 
+import GoogleCallback from './pages/GoogleCallback'; // 추가!
 
 const router = createBrowserRouter([
   { path: "/", element: <LoginPage /> },
   { path: "/login", element: <LoginPage /> },
   { path: "/signup", element: <SignupPage /> },
-
   
+
+  { path: "/v1/auth/google/callback", element: <GoogleCallback /> },
+
   {
     element: <ProtectedRoute />, 
     children: [
       { path: "/mypage", element: <MyPage /> },
-    
     ],
   },
 ]);

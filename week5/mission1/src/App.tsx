834c4badddf7b45f582e68b88d1ff46d254106df
이윤정 <jjungjjung0326@naver.com>
2026-05-08 18:06:19 +0900

    5주차 최종 완료

--- week5/mission1/src/App.tsx
+++ week5/mission1/src/App.tsx
@@ -1,4 +1,4 @@
-import "./App.css";
+
 import{createBrowserRouter, RouterProvider} from "react-router-dom";
 import type {RouteObject} from "react-router-dom";
 import HomePage from "./pages/HomePage"

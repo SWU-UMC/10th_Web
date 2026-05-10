6f46bf9a2a504980d392a95e9a696996ddc13502
이윤정 <jjungjjung0326@naver.com>
2026-05-10 23:49:01 +0900

    6주차 미션 2 완료

---
+++ week6/mission2/src/components/LpCardSkeletonList.tsx
@@ -0,0 +1,14 @@
+import LpCardSkeleton from "./LpCard/LpCardSkeleton";
+interface LpCardSkeletonListProps{
+    count:number;
+}
+const LpCardSkeletonList=({ count }:LpCardSkeletonListProps)=>{
+    return(
+        <>
+        {new Array(count).fill(0).map((_,idx)=>(
+            <LpCardSkeleton key={idx}/>
+        ))}
+        </>
+    );
+};
+export default LpCardSkeletonList;

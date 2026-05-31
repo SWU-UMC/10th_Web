276f02075dd7700922f18887f52906436e9f88bd
이윤정 <jjungjjung0326@naver.com>
2026-05-31 22:21:38 +0900

    9주차 미션 1 완료

---
+++ week9/src/components/CartItems.tsx
@@ -0,0 +1,55 @@
+import { useDispatch } from "react-redux";
+import type { Lp } from "../types/cart";
+import {increase, decrease} from "../store/slices/cartSlice";
+
+
+interface CartItemProps {
+  lp: Lp;
+}
+
+const CartItem = ({ lp }: CartItemProps) => {
+  const dispatch=useDispatch();
+  const handleIncrease=()=>{
+    dispatch(increase({id: lp.id}));
+  };
+  const handleDecrease=()=>{
+    if(lp.amount===1){
+      dispatch(decrease({id: lp.id}));
+      return;
+    }
+    
+  };
+  return (
+    <div className='flex items-center p-4 border-b border-gray-200'>
+      <img
+        src={lp.img}
+        alt={`${lp.title}의 LP 이미지`}
+        className='w-20 h-20 object-cover rounded mr-4'
+      />
+
+      <div className='flex-1'>
+        <h3 className='text-xl font-semibold'>{lp.title}</h3>
+        <p className='text-sm text-gray-600'>{lp.singer}</p>
+        <p className='text-sm font-bold text-gray-600'>
+          \{lp.price} 원
+        </p>
+      </div>
+
+      <div className='flex items-center'>
+        <button onClick={handleDecrease} className='px-3 py-1 bg-gray-300 text-gray-800 rounded-l hover:bg-gray-400 cursor-pointer'>
+          -
+        </button>
+
+        <span className='px-4 py-[3px] border-y border-gray-300'>
+          {lp.amount}
+        </span>
+
+        <button onClick={handleIncrease} className='px-3 py-1 bg-gray-300 text-gray-800 rounded-r hover:bg-gray-400 cursor-pointer'>
+          +
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default CartItem;
---
+++ week9/src/store/slices/cartSlice.ts
@@ -0,0 +1,72 @@
+import cartItems from '../../constants/cartltems';
+import type {CartItems} from '../../types/cart';
+import {createSlice, type PayloadAction} from "@reduxjs/toolkit";
+//초기상태의 interface
+export interface CartState {
+    cartItems: CartItems;
+    amount: number;
+    total: number;
+}
+const initialState:CartState={
+    cartItems: cartItems,
+    amount:0,
+    total:0,
+}
+//cartSlice 생성
+//createSlice->reduxToolkit에서 제공
+const cartSlice=createSlice({
+    name:'cart',
+    initialState,
+    reducers:{
+        //Todo: 증가
+        increase:(state, action:PayloadAction<{id:string}>)=>{
+            const itemId=action.payload.id;
+            //이 아디이를 통해서 전체 음반 중 내가 클릭한 음반을 찾기
+            const item=state.cartItems.find((cartItem)=>cartItem.id===itemId);
+            if(item){
+                item.amount+=1;
+            }
+        },
+       
+        //Todo: 감소
+        decrease:(state, action:PayloadAction<{id:string}>)=>{
+            const itemId=action.payload.id;
+            //이 아디이를 통해서 전체 음반 중 내가 클릭한 음반을 찾기
+            const item=state.cartItems.find((cartItem)=>cartItem.id===itemId);
+            if(item){
+                item.amount -=1;
+            }
+        },
+        //Todo: removeItem 제거
+        removeItem:(state, action:PayloadAction<{id:string}>)=>{
+            //아이템 찾기
+            const itemId=action.payload.id;
+            //그것 빼고 나머지 보여줘야 함
+            state.cartItems=state.cartItems.filter(
+                (cartItem)=>cartItem.id!==itemId);
+            
+        },
+        //Todo: clearCart 장바구니 비우기
+        clearCart:(state)=>{
+            state.cartItems=[];
+        },
+        //Todo: 총액 계산
+        calcultateTotals:(state)=>{
+            let amount =0;
+            let total=0;
+            state.cartItems.forEach((item):void=>{
+                amount+=item.amount;
+                total +=item.amount*item.price;
+            })
+            state.amount=amount;
+            state.total=total;
+        }
+
+    }
+});
+export const {increase, decrease, removeItem, clearCart, calcultateTotals}=cartSlice.actions;
+//duck patther reducer는 export default로 내보내야 함
+const cartReducer=cartSlice.reducer;
+
+export default cartReducer;
+
